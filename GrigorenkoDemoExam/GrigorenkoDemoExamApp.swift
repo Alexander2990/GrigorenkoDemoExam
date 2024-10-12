c75c4db0a74132396853e7841bc39ffd8d7722c2
irina050890 <birinka007@mail.ru>
2024-10-12 09:45:53 +0700

    Initial Commit

---
+++ GrigorenkoDemoExam/GrigorenkoDemoExamApp.swift
@@ -0,0 +1,17 @@
+//
+//  GrigorenkoDemoExamApp.swift
+//  GrigorenkoDemoExam
+//
+//  Created by user on 12.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct GrigorenkoDemoExamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
