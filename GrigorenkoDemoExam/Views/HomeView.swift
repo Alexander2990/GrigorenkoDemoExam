7e4171d2f848f581d92269c3c8bde7620c56df19
Alexander2990 <Alexander29_90@icloud.com>
2024-10-12 10:26:37 +0700

    Added HomeView

---
+++ GrigorenkoDemoExam/Views/HomeView.swift
@@ -0,0 +1,19 @@
+//
+//  HomeView.swift
+//  GrigorenkoDemoExam
+//
+//  Created by user on 12.10.2024.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Home")
+    }
+}
+
+#Preview {
+    HomeView()
+}
+
